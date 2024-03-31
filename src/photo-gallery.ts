b7658101af7ab5c121f49e048f2d234ed851f7da
Freddy Ewald <freddiemailster@gmail.com>
2024-03-30 21:46:40 -0700

    initialize delay

--- src/photo-gallery.ts
+++ src/photo-gallery.ts
@@ -1,9 +1,14 @@
 import { LitElement, css, html } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement('photo-gallery')
 export class PhotoGallery extends LitElement {
   static override styles = css`
+    /** Needed because display:flex overrides hidden */
+    [hidden] {
+      display: none !important;
+    }
+
     #photoGallery {
       background-color: cyan;
       position: relative;
@@ -26,18 +31,53 @@ export class PhotoGallery extends LitElement {
       top: 0;
       left: 0;
     }
-    #controls>a {
+    #controls > a {
       background-color: honeydew;
     }
   `;
 
+  @property({type: Number})
+  delay?: number
+  private _delayInterval?: number
+
+
+  @property({type: Boolean})
+  showControls: boolean = false;
+
+  private _nextImage(e: MouseEvent) {
+    e.preventDefault();
+    // TODO: Implement
+  }
+
+  private _previousImage(e: MouseEvent) {
+    e.preventDefault();
+    // TODO: Implement
+  }
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    if (this.delay) {
+      this._delayInterval = setInterval(() => {
+        console.log('next image');
+      }, this.delay);
+    }
+  }
+
+  override disconnectedCallback(): void {
+    super.disconnectedCallback();
+    if (this._delayInterval) {
+      clearInterval(this._delayInterval);
+    }
+  }
+
   protected override render() {
+    console.log(this.showControls);
     return html`
       <div id="photoGallery">
         <img src="/dev/picture1.jpg" />
-        <div id="controls">
-          <a href="#previous">Previous</a>
-          <a href="#next">Next</a>
+        <div ?hidden=${!this.showControls} id="controls">
+          <a @click="${this._previousImage}" href="#previous">Previous</a>
+          <a @click="${this._nextImage}" href="#next">Next</a>
         </div>
         <slot></slot>  
       </div>
