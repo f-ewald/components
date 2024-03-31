eae16b7c71277f9d8851d08df704feaa55afa5cc
Freddy Ewald <freddiemailster@gmail.com>
2024-03-30 21:05:40 -0700

    Fix controls

--- src/photo-gallery.ts
+++ src/photo-gallery.ts
@@ -6,22 +6,25 @@ export class PhotoGallery extends LitElement {
   static override styles = css`
     #photoGallery {
       background-color: cyan;
+      position: relative;
+      display: block;
     }
 
-    #container {
-      background-color: aliceblue;
-      height: 12rem;
-      display: flex;
-      /* flex-grow: 1; */
-      /* align-content: center; */
+    #photoGallery > img {
+      display: block;
+      width: 100%;
     }
 
     #controls {
-      z-index: 10;
+      position: absolute;
       display: flex;
       flex-grow: 1;
       justify-content: space-between;
       align-items: center;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
     }
     #controls>a {
       background-color: honeydew;
@@ -31,12 +34,10 @@ export class PhotoGallery extends LitElement {
   protected override render() {
     return html`
       <div id="photoGallery">
-        <div id="container">
-          <!-- <img src="/dev/picture1.jpg" /> -->
-          <div id="controls">
-            <a href="#previous">Previous</a>
-            <a href="#next">Next</a>
-          </div>
+        <img src="/dev/picture1.jpg" />
+        <div id="controls">
+          <a href="#previous">Previous</a>
+          <a href="#next">Next</a>
         </div>
         <slot></slot>  
       </div>
